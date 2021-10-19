73255ddb42a57443cfc2eb5e15793db84dac8815
mtk246 <minthukyaw454@gmail.com>
2021-10-19 11:14:05 +0630

    Final not final

--- index.php
+++ index.php
@@ -1,216 +1,151 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta property="og:image" content="img/1.jpg" />
-    <title>Happy Thadingyut by KoPwar</title>
 
-    <!-- Bootstrap CSS CDN -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-      crossorigin="anonymous"
-    />
-    <!-- Custom Css -->
-    <link rel="stylesheet" href="styles.css" type="text/css" />
-    <!-- Font Awesome JS -->
-    <script
-      defer
-      src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
-      integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      defer
-      src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
-      integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
-      crossorigin="anonymous"
-    ></script>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta property="og:image" content="img/1.jpg" />
+  <title>Happy Thadingyut by KoPwar</title>
 
-    <!-- Google fonts-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,600,800&amp;display=swap">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- Open Sans -->
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-    <!-- Source Sans Pro -->
-    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@1,900&display=swap" rel="stylesheet">
-    <!-- Lato -->
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,900&display=swap" rel="stylesheet">
-    <!-- Ubuntu -->
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
-    <!-- Poppins -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
-    <!-- Noto Sans Font-->
-    <link
-      href="https://fonts.googleapis.com/css2?family=Noto+Serif+Myanmar:wght@600&display=swap"
-      rel="stylesheet"
-    />
+  <!-- Bootstrap CSS CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
+  <!-- Custom Css -->
+  <link rel="stylesheet" href="styles.css" type="text/css" />
+  <!-- Font Awesome JS -->
+  <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
 
-   <!-- Favicon-->
-   <link rel="shortcut icon" href="img/1.jpg">
-  </head>
-  <body>
-    <img
-            src="img/1.jpg"
-            alt="kopwar_logo"
-            class="rounded-circle d-none"
-            width="70"
-        />
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
-      <div class="custom-nav container-fluid">
-        <a class="navbar-brand" href="#"
-          ><img
-            src="img/kopwar.jpg"
-            alt="kopwar_logo"
-            class="rounded-circle"
-            width="70"
-        /></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav text-center" style="margin-left: auto">
-            <a class="nav-link btn" aria-current="page" href="https://www.kopwar.com.mm"><p class="m-auto">Home</p></a>
-          </div>
+  <!-- Google fonts-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,600,800&amp;display=swap">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <!-- Open Sans -->
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+  <!-- Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@1,900&display=swap" rel="stylesheet">
+  <!-- Lato -->
+  <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,900&display=swap" rel="stylesheet">
+  <!-- Ubuntu -->
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+  <!-- Poppins -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+  <!-- Noto Sans Font-->
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Myanmar:wght@600&display=swap" rel="stylesheet" />
+
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="img/kopwar.jpg">
+</head>
+
+<body>
+  <img src="img/1.jpg" alt="kopwar_logo" class="rounded-circle d-none" width="70" />
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+    <div class="custom-nav container-fluid">
+      <a class="navbar-brand" href="#"><img src="img/kopwar.jpg" alt="kopwar_logo" class="rounded-circle" width="70" /></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav text-center" style="margin-left: auto">
+          <a class="nav-link btn" aria-current="page" href="https://www.kopwar.com.mm">
+            <p class="m-auto">Home</p>
+          </a>
         </div>
       </div>
-    </nav>
-    <div class="container text-center my-3 px-4 col-12 col-lg-6"> 
-        <h1 class="my-4 bg-light p-2 rounded-3 shadow-sm" style="color: #ff3e14;">Happy Thadingyut 2021</h1>
-      <img
-        src="img/1.jpg"
-        alt="Thadingyut"
-        class="img-fluid rounded-3"
-        width="400"
-      />
-      <p class="c-font my-4">
-        ကိုယ်ပွားကိုအစဥ်အမြဲအားပေးကြတဲ့ မိတ်ဟောင်းမိတ်သစ်များအားလုံး ဝါလကင်းလွတ်
-        သီတင်းကျွတ်အချိန်အခါသမယမှာ ချစ်ရတဲ့မိသားစုနဲ့အတူ စိတ်ချမ်းသာ
-        ကိုယ်ကျန်းမာနဲ့ ပျော်ရွှင်စွာဖြတ်သန်းနိုင်ပါစေကြောင်း
-        ဆုတောင်းမေတ္တာပို့သအပ်ပါတယ် 🙏🙏🙏 <br/><br/>ကိုယ်ပွားကမှာယူထားတဲ့
-        အော်ဒါအားလုံးကို သီတင်းကျွတ်လပြည့်နေ့မှာ(၁)ရက် ရပ်နားထားပြီး
-        ၂၁၊၁၀၊၂၀၂၁(ကြာသပတေးနေ့)မှာ ပုံမှန်အတိုင်း
-        ပြန်လည်ပို့ဆောင်ပေးမှာဖြစ်ကြောင်း ချစ်မိတ်ဆွေတို့ကို သတင်းပါးအပ်ပါတယ်။
-        <br />🌌🌕🎉🎇
-      </p>
     </div>
-  </body>
-  <footer class="hero-footer with-pattern-1 position-relative">
-    <div class="container-fluid col-10 py-5 w-85" id="contact">
-      <div class="row">
-        <div class="col-lg-4 mb-4 mb-lg-0">
-          <img
-            class="rounded-circle mb-4"
-            src="img/kopwar.jpg"
-            alt=""
-            width="60"
-          />
-          <p class="text-muted" style="font-size: 1rem">
-            <span
-              class="span-style"
-              style="
+  </nav>
+  <div class="container text-center my-3 px-4 col-12 col-lg-6">
+    <h1 class="my-4 bg-light p-2 rounded-3 shadow-sm" style="color: #ff3e14;">Happy Thadingyut 2021</h1>
+    <img src="img/1.jpg" alt="Thadingyut" class="img-fluid rounded-3" width="400" />
+    <p class="c-font my-4">
+      ကိုယ်ပွားကိုအစဥ်အမြဲအားပေးကြတဲ့ မိတ်ဟောင်းမိတ်သစ်များအားလုံး ဝါလကင်းလွတ်
+      သီတင်းကျွတ်အချိန်အခါသမယမှာ ချစ်ရတဲ့မိသားစုနဲ့အတူ စိတ်ချမ်းသာ
+      ကိုယ်ကျန်းမာနဲ့ ပျော်ရွှင်စွာဖြတ်သန်းနိုင်ပါစေကြောင်း
+      ဆုတောင်းမေတ္တာပို့သအပ်ပါတယ် 🙏🙏🙏 <br /><br />ကိုယ်ပွားကမှာယူထားတဲ့
+      အော်ဒါအားလုံးကို သီတင်းကျွတ်လပြည့်နေ့မှာ(၁)ရက် ရပ်နားထားပြီး
+      ၂၁၊၁၀၊၂၀၂၁(ကြာသပတေးနေ့)မှာ ပုံမှန်အတိုင်း
+      ပြန်လည်ပို့ဆောင်ပေးမှာဖြစ်ကြောင်း ချစ်မိတ်ဆွေတို့ကို သတင်းပါးအပ်ပါတယ်။
+      <br />🌌🌕🎉🎇
+    </p>
+  </div>
+</body>
+<footer class="hero-footer with-pattern-1 position-relative">
+  <div class="container-fluid col-10 py-5 w-85" id="contact">
+    <div class="row">
+      <div class="col-lg-4 mb-4 mb-lg-0">
+        <img class="rounded-circle mb-4" src="img/kopwar.jpg" alt="" width="60" />
+        <p class="text-muted" style="font-size: 1rem">
+          <span class="span-style" style="
                 color: #ff3e14;
                 font-weight: bold;
                 font-size: 1.5rem;
                 font-style: italic;
-              "
-              >KOPWAR</span
-            >
-            &nbsp;is an online store. Begin as a foodservice provider to
-            business and other service. By using our app, you will never need to
-            go to Market and get your hand dirty.
-          </p>
-        </div>
+              ">KOPWAR</span>
+          &nbsp;is an online store. Begin as a foodservice provider to
+          business and other service. By using our app, you will never need to
+          go to Market and get your hand dirty.
+        </p>
+      </div>
 
-        <div class="col-lg-4 mb-4" style="font-size: 1.1rem">
-          <img src="img/contacts.svg" alt="contact" width="50" class="mb-4" />
-          <h4
-            class="mb-4 span-style"
-            style="font-size: 1.8rem; font-weight: bold; color: #ff3e14"
-          >
-            Contact Info
-          </h4>
-          <ul class="list-unstyled mr-4 mb-3">
-            <li class="mb-2 text-muted">
-              No.953, Yar Za Thingyan Street 10th mile, Insein Township, Yangon.
-            </li>
-            <li class="mb-2">
-              <a class="footer-link" href="tel:09447407050">014707050</a>
-            </li>
-            <li class="mb-2">
-              <a class="footer-link" href="mailto:sane.l.wunn@gmail.com"
-                >sane.l.wunn@gmail.com</a
-              >
-            </li>
-          </ul>
-          <ul class="list-inline my-4">
-            <li class="list-inline-item">
-              <a class="social-link" href="https://www.facebook.com/kopwarmm"
-                ><img src="img/facebook.png" alt="facebook" width="30"
-              /></a>
-            </li>
-            <li class="list-inline-item">
-              <a class="social-link" href="#">
-                <img src="img/messenger.png" alt="messenger" width="30" />
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a class="social-link" href="tel: 09447407050"></a>
-                <img src="img/phone.png" alt="phone" width="30" />
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a class="social-link" href="mailto: sane.l.wunn@gmail.com">
-                  <img src="img/email.png" alt="email" width="30" />
+      <div class="col-lg-4 mb-4" style="font-size: 1.1rem">
+        <img src="img/contacts.svg" alt="contact" width="50" class="mb-4" />
+        <h4 class="mb-4 span-style" style="font-size: 1.8rem; font-weight: bold; color: #ff3e14">
+          Contact Info
+        </h4>
+        <ul class="list-unstyled mr-4 mb-3">
+          <li class="mb-2 text-muted">
+            No.953, Yar Za Thingyan Street 10th mile, Insein Township, Yangon.
+          </li>
+          <li class="mb-2">
+            <a class="footer-link" href="tel:09447407050">014707050</a>
+          </li>
+          <li class="mb-2">
+            <a class="footer-link" href="mailto:sane.l.wunn@gmail.com">sane.l.wunn@gmail.com</a>
+          </li>
+        </ul>
+        <ul class="list-inline my-4">
+          <li class="list-inline-item">
+            <a class="social-link" href="https://www.facebook.com/kopwarmm"><img src="img/facebook.png" alt="facebook" width="30" /></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link" href="#">
+              <img src="img/messenger.png" alt="messenger" width="30" />
             </a>
-            </li>
-          </ul>
-        </div>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link" href="tel: 09447407050"></a>
+            <img src="img/phone.png" alt="phone" width="30" />
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link" href="mailto: sane.l.wunn@gmail.com">
+              <img src="img/email.png" alt="email" width="30" />
+            </a>
+          </li>
+        </ul>
+      </div>
 
-        <div class="col-lg-4">
-          <img src="img/map.svg" alt="contact" width="50" class="mb-4" />
-          <h4
-            class="mb-4 span-style"
-            style="font-size: 1.8rem; font-weight: bold; color: #ff3e14"
-          >
-            Our Location
-          </h4>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3817.730633625296!2d96.12430761488201!3d16.889214421426473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1958b78392939%3A0xb1e3dd5e28a4ad04!2sKOPWAR!5e0!3m2!1sen!2smm!4v1632892422355!5m2!1sen!2smm"
-            style="border: 0; width: 100%; height: auto"
-            allowfullscreen=""
-            loading="lazy"
-          ></iframe>
-        </div>
+      <div class="col-lg-4">
+        <img src="img/map.svg" alt="contact" width="50" class="mb-4" />
+        <h4 class="mb-4 span-style" style="font-size: 1.8rem; font-weight: bold; color: #ff3e14">
+          Our Location
+        </h4>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3817.730633625296!2d96.12430761488201!3d16.889214421426473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1958b78392939%3A0xb1e3dd5e28a4ad04!2sKOPWAR!5e0!3m2!1sen!2smm!4v1632892422355!5m2!1sen!2smm" style="border: 0; width: 100%; height: auto" allowfullscreen="" loading="lazy"></iframe>
       </div>
     </div>
-    <div class="copyrights">
-      <div class="container text-center py-4">
-        <p class="mb-0 text-muted text-sm">Copyright © Kopwar Website 2021.</p>
-      </div>
+  </div>
+  <div class="copyrights">
+    <div class="container text-center py-4">
+      <p class="mb-0 text-muted text-sm">Copyright © Kopwar Website 2021.</p>
     </div>
-  </footer>
+  </div>
+</footer>
 
-  <!-- AJAX -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <!-- Popper.JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"></script>
-  <!-- Bootstrap JS -->
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
-  ></script>
+<!-- AJAX -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
